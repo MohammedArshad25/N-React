5d3d86ec661776cd8f4e6dc6b923e06095f87c92
Arshad <arshadshaik4325@gmail.com>
2024-07-09 12:39:22 +0530

    creating a bundler using parcel

--- E-1/HelloWorld/App.js
+++ E-1/HelloWorld/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", {id : "head1"}, "Hello world using React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
